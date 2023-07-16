2af75dda4a0912a42a5b443ff80a3b8aa5f0cd92
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-07-16 12:07:56 +0300

    Updates

--- get_data.php
+++ get_data.php
@@ -3,10 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-$db_host = 'kra4404859.mysql';
-$db_user = 'kra4404859_mysql';
-$db_password = 'GMk5mo/B';
-$db_name = 'kra4404859_db';
+$db_host = $_SERVER['SECRET_DB_HOST'];
+$db_user = $_SERVER['SECRET_DB_USER'];
+$db_password = $_SERVER['SECRET_DB_PASSWORD'];
+$db_name = $_SERVER['SECRET_DB_NAME'];
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
@@ -19,8 +19,9 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $data = $result->fetch_all(MYSQLI_ASSOC);
-    header('Content-Type: application/json');
-    echo json_encode($data);
+    $json_data = json_encode($data);
+    file_put_contents('data.json', $json_data);
+    echo $json_data;
 } else {
     echo "0 результатов";
 }
