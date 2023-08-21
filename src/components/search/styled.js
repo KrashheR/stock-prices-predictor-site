9af1062b4229cdd5eeccb2cb36e6ebab64b14251
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-21 21:01:25 +0300

    Styled search component

--- src/components/search/styled.js
+++ src/components/search/styled.js
@@ -1,13 +1,28 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.input`
-  border-radius: 20px;
   padding-left: 10px;
-  outline: 0;
+  padding: 10px;
+  border: 2px solid ${props => props.theme.colors.primary};
+  border-radius: 20px;
+  outline: 2px solid ${props => props.theme.colors.primary};
+  font-family: Montserrat;
   font-size: ${props => props.theme.fontSizes.small};
+  background-color: ${props => props.theme.colors.bg};
+  color: ${props => props.theme.colors.text};
+  transition: all 0.2s ease;
+
+  &:focus{
+    outline: 2px solid ${props => props.theme.colors.text};
+  }
 
   @media (max-width: ${ props => props.theme.deviceSizes.laptop }) {
-    width: 200px;
-    height: 35px;
+    width: 300px;
+    height: 45px;
+  }
+
+  @media (max-width: ${ props => props.theme.deviceSizes.mobile }) {
+    width: 240px;
+    height: 45px;
   }
 `;
